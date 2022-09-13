410e9341ca8b591ec80d74724997506d41ae487d
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-13 09:10:01 -0300

    iniciei o bubble sort

---
+++ algorithms/bubble_sort.rb
@@ -0,0 +1,13 @@
+# https://www.geeksforgeeks.org/bubble-sort/
+
+def bubble_sort (lista)
+    p "A lista a ser ordenada é"
+    p lista
+
+
+
+end
+
+
+#lista = [5, 1, 4, 2, 8]
+p bubble_sort([5, 1, 4, 2, 8])
