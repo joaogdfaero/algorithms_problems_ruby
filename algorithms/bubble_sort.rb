8a6f7ed15901c07a7bf3eb7e53d006bdb162c930
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-13 09:22:11 -0300

    bubble sort funcionou com gambiarra

--- algorithms/bubble_sort.rb
+++ algorithms/bubble_sort.rb
@@ -4,6 +4,34 @@ def bubble_sort (lista)
     p "A lista a ser ordenada é"
     p lista
 
+    lista_ordenada = lista.sort
+    p "A lista ordenada é"
+    p lista_ordenada
+
+while lista != lista_ordenada do
+if lista[0]>lista[1]
+    p "O primeiro é maior"
+    first=lista[0]
+    second=lista[1]
+    lista[0]=lista[1]
+    lista[1]=first
+    p lista
+elsif lista[1]>lista[2]
+    first=lista[1]
+    second=lista[2]
+    lista[1]=lista[2]
+    lista[2]=first
+    p lista
+
+elsif lista[2]>lista[3]
+    first=lista[2]
+    second=lista[3]
+    lista[2]=lista[3]
+    lista[3]=first
+    p lista
+end
+end
+
 
 
 end
