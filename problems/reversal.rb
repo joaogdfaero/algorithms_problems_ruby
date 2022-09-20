26d54a69e10b1bfdd1983d08ca1d496ec677cbcf
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-20 10:49:33 -0300

    faz a troca mas não tá funcionando direito

--- problems/reversal.rb
+++ problems/reversal.rb
@@ -2,15 +2,18 @@
 
 # Given an array, right rotate it by k elements. 
 
-# Input: arr[] = {1, 2, 3, 4, 5, 
-#     6, 7, 8, 9, 10}
+# Input: arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 # k = 3
 # Output: 8 9 10 1 2 3 4 5 6 7
 
 def reversal(list,k)
+    p "A lista inicialmente é"
     p list
-    p k
-    
+    tamanho = list.length
+    (0..tamanho).each do |i|
+       list[i],list[i+3] = list[i+3], list[i]
+       p list
+    end 
 end
 
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
