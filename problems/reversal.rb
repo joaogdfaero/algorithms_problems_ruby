9c04c455a19c88bb8963035576356b37830bbd91
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-20 10:39:49 -0300

    criado arquivo do reversal

---
+++ problems/reversal.rb
@@ -0,0 +1,18 @@
+# Reversal algorithm for right rotation of an array
+
+# Given an array, right rotate it by k elements. 
+
+# Input: arr[] = {1, 2, 3, 4, 5, 
+#     6, 7, 8, 9, 10}
+# k = 3
+# Output: 8 9 10 1 2 3 4 5 6 7
+
+def reversal(list,k)
+    p list
+    p k
+    
+end
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+k = 3
+reversal(list,k)
