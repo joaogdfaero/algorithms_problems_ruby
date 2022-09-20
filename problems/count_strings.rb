cdfa3bf67fe10e5f289503a5379d12b1918051b8
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-20 09:14:05 -0300

    Upper Case e Lower Case funcionando

--- problems/count_strings.rb
+++ problems/count_strings.rb
@@ -16,15 +16,14 @@ def count(input)
     p "All upper case letters are"
     alphabet_u = ('A'..'Z').to_a # alphabeto
     p alphabet_u
-    p "O tamanho_u é"
     tamanho_u = alphabet_u.length
-    p tamanho_u
     count_u = 0
 
     p "All lower case letters are"
     alphabet_l = ('a'..'z').to_a # alphabeto
     p alphabet_l
     tamanho_l = alphabet_l.length
+    count_l = 0
 
     p "All numbers are"
     numbers = (0..9).to_a
@@ -34,12 +33,23 @@ def count(input)
     #p "All special characters are"
 
     (0..tamanho_u-1).to_a.each do |i|
-        if input.include? alphabet_u[i]
+        if alphabet_u.include? input[i]
             count_u = count_u + 1
         end
     end
+
     p "Upper Case Letters:"
-    return count_u
+    p count_u
+
+    (0..tamanho_l-1).to_a.each do |i|
+        #if input.include? alphabet_l[i]
+        if alphabet_l.include? input[i]
+            count_l = count_l + 1
+        end
+    end
+    p "Lower Case Letters:"
+    p count_l
+
         
 end
 
