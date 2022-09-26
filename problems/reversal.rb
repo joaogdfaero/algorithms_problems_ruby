da99785cb69d10ffc9aae771beeca728428dcc3d
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-26 14:44:06 -0300

    rodando mas faltou a igualdade dupla

--- problems/reversal.rb
+++ problems/reversal.rb
@@ -6,6 +6,7 @@
 # k = 3
 # Output: 8 9 10 1 2 3 4 5 6 7
 
+
 def reversal(list,k)
     p "A list inicialmente é"
     p list
@@ -14,26 +15,18 @@ def reversal(list,k)
 
     # "troca os 3 primeiros com os 3 do meio"  
     tamanho = list.length
-    (0..(tamanho/3).to_int-1).each do |i|
-       list[i],list[i+3] = list[i+3], list[i]
-       p list
+    (0..tamanho-1).each do |i|
+        if i < tamanho -k -1 # Only for the first length - k numbers            
+            #Parei aqui
+           list[i] = list[i+k]
+           p "front"
+           p list
+        else
+           list[i] = list [i-(tamanho-k-1)]
+           p "back"
+           p list
+        end
     end 
-
-
-    # coloca o 7 no final
-    list[6],list[9]=list[9],list[6]
-    p "Arrumando o final"
-    p list
-    list[6],list[8]=list[8],list[6]
-    p list
-    list[6],list[7]=list[7],list[6]
-
-    # finalizando
-    (0..2).each do |i|
-        list[i],list[i+6] = list[i+6],list[i]
-    end
-    p "A lista final é"
-    p list
 end
 
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
