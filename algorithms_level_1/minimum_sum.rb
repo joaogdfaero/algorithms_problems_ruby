a0a7531ee868a0b8655fb142c56ee2aa1a7bf675
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-29 17:43:28 -0300

    feat: deleting from A and adding to B

--- algorithms_level_1/minimum_sum.rb
+++ algorithms_level_1/minimum_sum.rb
@@ -1,15 +1,38 @@
-# https://www.geeksforgeeks.org/minimum-sum-choosing-minimum-pairs-array/ (read kujikuji comment for better explanation)
+# https://www.geeksforgeeks.org/minimum-sum-choosing-minimum-pairs-A/ (read kujikuji comment for better explanation)
 
 # form two digit numbers in a way the the sum of this two numbers is minimum
 
 #Input : A[] = {2, 4, 1, 3}
 #Output : 3
 
+# So the minimum sum can always be (minimum of A[]) * (size of B[] (which equals n-1)).
+
+
+
 def minimum_sum(array)
-    p array
+    p "A = #{A}"
+
+    B_array =[0]
+
+    p A.length
+# until A.lenght = 1
+    (0..A.length).each do |i|
+        if A[i]>A[i+1]
+            A.delete_at(i)
+            B.append(A[i+1])
+            p "B é:"
+            p B
+        elsif A[i]<A[i+1]
+            A.delete_at(i+1)
+            B.append(A[i])
+            p "B é:"
+            p B
+        end
+    end
+
 end
 
-array = [2,4,1,3]
-minimum_sum(array)
+A = [2,4,1,3]
+minimum_sum(A)
 
 
