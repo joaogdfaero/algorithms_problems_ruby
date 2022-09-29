2fcbe096b7a2b6391cca3f6f57d6bc534b75826d
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-29 17:57:12 -0300

    não pega sempre o primeiro, por isso não funciona

--- algorithms_level_1/minimum_sum.rb
+++ algorithms_level_1/minimum_sum.rb
@@ -1,38 +1,45 @@
-# https://www.geeksforgeeks.org/minimum-sum-choosing-minimum-pairs-A/ (read kujikuji comment for better explanation)
+# https://www.geeksforgeeks.org/minimum-sum-choosing-minimum-pairs-a/ (read kujikuji comment for better explanation)
 
 # form two digit numbers in a way the the sum of this two numbers is minimum
 
-#Input : A[] = {2, 4, 1, 3}
+#Input : a[] = {2, 4, 1, 3}
 #Output : 3
 
-# So the minimum sum can always be (minimum of A[]) * (size of B[] (which equals n-1)).
+# So the minimum sum can always be (minimum of a[]) * (size of b[] (which equals n-1)).
 
 
 
-def minimum_sum(array)
-    p "A = #{A}"
+def minimum_sum(a)
+    p "a = #{a}"
 
-    B_array =[0]
+    b =[]
+    
+    size = a.length
+    p size
 
-    p A.length
-# until A.lenght = 1
-    (0..A.length).each do |i|
-        if A[i]>A[i+1]
-            A.delete_at(i)
-            B.append(A[i+1])
-            p "B é:"
-            p B
-        elsif A[i]<A[i+1]
-            A.delete_at(i+1)
-            B.append(A[i])
-            p "B é:"
-            p B
+    until size == 1 do
+        (0..a.length).each do |i|
+            if a[i]>a[i+1]
+                a.delete_at(i)
+                b.append(a[i+1])
+                p "b é:"
+                p b
+                p "a é"
+                p a
+            elsif a[i]<a[i+1]
+                a.delete_at(i+1)
+                b.append(a[i])
+                p "b é:"
+                p b
+                p "a é"
+                p a
+            end
         end
     end
 
 end
 
-A = [2,4,1,3]
-minimum_sum(A)
+a = [2,4,1,3]
+minimum_sum(a)
 
 
