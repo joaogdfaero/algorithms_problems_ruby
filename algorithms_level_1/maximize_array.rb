d8d67b0091b1b1a746c63be786c6c5edd90d29be
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-30 06:59:10 -0300

    feat: find minimum

--- algorithms_level_1/maximize_array.rb
+++ algorithms_level_1/maximize_array.rb
@@ -1,6 +1,12 @@
 # https://www.geeksforgeeks.org/maximize-array-sun-after-k-negation-operations/
 
 def maximize_array(a,k)
+    # 1 find minimum number in array
+    # 2 turn that number to number = - number
+    # 3 do that k times
+    # 4 sum all numbers in the final array
+
+    p a.min
 
 end
 
