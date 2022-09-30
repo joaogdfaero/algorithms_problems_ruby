54a77784c4748a4ea805a5c4bea7dcd5b26ba21f
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-30 07:21:13 -0300

    feat: failing when a[i]=0

--- algorithms_level_1/maximize_array.rb
+++ algorithms_level_1/maximize_array.rb
@@ -5,12 +5,22 @@ def maximize_array(a,k)
     # 2 turn that number to number = - number
     # 3 do that k times
     # 4 sum all numbers in the final array
+    p "A lista é #{a}"
 
-    (0..k-1).each do |i|  # isn't working because the code isn't switching the number k times
+    count = 0 # stops when count = k
+
+    (0..100).each do |i|  
         p "o mínimo é #{a.min}"
-        if a[i] == a.min
+        if a[i] == a.min # isn't working because the code isn't entering this loop when a[i] == 0
             a[i] = - a[i]
+            count = count +1
+            p "count é #{count}"
+            p "k é #{k}"
             p "a agora é #{a}"
+            if count == k
+                p "O K MÁXIMO FOI ALCANÇADO"
+                break # stops everthing when k is reached
+            end
         end
     end
 
