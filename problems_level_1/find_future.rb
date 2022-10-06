79319d65a189b5cf6fa4a3f95014bd52e1a28d8b
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-06 07:38:31 -0300

    feat: started find future

---
+++ problems_level_1/find_future.rb
@@ -0,0 +1,9 @@
+# https://www.geeksforgeeks.org/queries-to-find-the-future-closest-date/
+
+
+def find_future(a,q)
+    
+end
+
+a = [“22/4/1233”, “1/3/633”, “23/5/56645”, “4/12/233”]
+q = [“23/3/4345”, “12/3/2”]
