a9de90551750c30dfacb2dbdd5c63ef99602b0f7
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-06 08:30:56 -0300

    feat: year difference for 2 dates working

--- problems_level_1/find_future.rb
+++ problems_level_1/find_future.rb
@@ -6,7 +6,7 @@ def find_future(a,q)
     a2 = []
     a3 = []
     a4 = []
-    arrays = [a1 , a2 , a3 ,a4] # arrays que vão conter a posição de /
+    arrays = [a1,a2,a3,a4] # arrays que vão conter a posição de / para a
 
      (0..a.length-1).each do |i|
         (0..a[i].length-1).each do |j|
@@ -15,11 +15,30 @@ def find_future(a,q)
                 arrays[i].append(j)
                 p arrays
             end
-        end
-        
+        end  
      end
 
-     # Year difference
+     q1 = []
+     q2 = []
+     qarrays = [q1,q2]
+
+     (0..q.length-1).each do |i|
+        (0..q[i].length).each do |j|
+            if q[i][j] == "/"
+                p "Achou o / no item #{i} de q na posição #{j}"
+                qarrays[i].append(j)
+                p qarrays
+            end
+        end
+     end
+
+     # year difference
+     year_a1 = a[0][arrays[0][1]+1..a[0].length-1].to_i
+     year_q1 = q[0][qarrays[0][1]+1..q[0].length-1].to_i
+     p year_a1 - year_q1
+
+
+
 
     
 end
