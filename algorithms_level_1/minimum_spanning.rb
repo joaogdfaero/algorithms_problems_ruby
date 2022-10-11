50933ba885f6c2fa53ef169e778770b02a5e202c
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-11 10:29:58 -0300

    docs: explanation written

---
+++ algorithms_level_1/minimum_spanning.rb
@@ -0,0 +1,8 @@
+# Vertice = número de bolas
+# Edges = conexões entre as bolas
+
+# Minimum spanning tree terá vertices - 1 de edges
+
+# Seleciona o caminho com menor custo entre as vértices
+# Seleciona o próximo caminho com menor custo, desde que ele não forme um ciclo (não feche o caminho)
+# Repete esse processo até EDGES do caminho = VERTICES - 1
