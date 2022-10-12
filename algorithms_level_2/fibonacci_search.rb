a4b7b9b6060be292088d8971735257304dd60b9d
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-12 12:10:30 -0300

    docs(fibonacci_search): calculated distance, but can't find minimum closest yet

--- algorithms_level_2/fibonacci_search.rb
+++ algorithms_level_2/fibonacci_search.rb
@@ -1,12 +1,20 @@
 # https://www.geeksforgeeks.org/fibonacci-search/
 
-
-
-
 def fibonacci_search(array,x,n)
   fibonacci_numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
 
-  distance = Array.new(n,0) #array to store distance between numbers
+  # find smallest closest fibonacci number
+  distance = Array.new(fibonacci_numbers.length,0) #array to store distance between numbers
+  (0..fibonacci_numbers.length-1).each do |i|
+    distance[i]=fibonacci_numbers[i]-n
+  end
+
+  (0..fibonacci_numbers.length-1).each do|i|
+    if distance[i] < 0
+      distance[i] = 1/0.0
+    end
+  end
+  p distance.min_by
 
 
   # m_th
