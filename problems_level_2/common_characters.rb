943eb5d79a69ef7424bc9471414c1168591106ac
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-12 13:41:43 -0300

    feat(common_characters): works finding included characters in each array

--- problems_level_2/common_characters.rb
+++ problems_level_2/common_characters.rb
@@ -2,14 +2,15 @@
 def common_characters(input)
   alphabet = ('a'..'z').to_a
 
-  included = Array.new(alphabet.length,0) # array that will be populated it included letters
+
+  included = Array.new(input.length){Array.new(alphabet.length,0)} # nested array that will be populated it included letters
+
   (0..alphabet.length-1).each do |i|
     (0..input.length-1).each do |j|
       if input[j].include?(alphabet[i])
-        included[i]=alphabet[i]
+        included[j][i] = alphabet[i]
       end
     end
-
   end
 
   p included
