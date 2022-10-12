75f397418a3c41d79106e8c919c634b50f1a2038
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-12 11:47:44 -0300

    feat(fibonacci_search): created distance array

--- algorithms_level_2/fibonacci_search.rb
+++ algorithms_level_2/fibonacci_search.rb
@@ -1,4 +1,23 @@
 # https://www.geeksforgeeks.org/fibonacci-search/
 
 
-fibonacci_numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+
+
+def fibonacci_search(array,x,n)
+  fibonacci_numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+
+  distance = Array.new(n,0) #array to store distance between numbers
+
+
+  # m_th
+  # m_th1
+  # m_th2
+
+
+end
+
+
+array = [2,3,4,10,40]
+x = 10
+n= array.length
+fibonacci_search(array,x,n)
