6d5d572764f1f92728c9157c4c639ca577146ff5
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-13 17:26:03 -0300

    feat(selection_sort): feature working!

--- algorithms_level_2/selection_sort.rb
+++ algorithms_level_2/selection_sort.rb
@@ -1,10 +1,9 @@
 def selection_sort(array)
 
-
   (0..array.length-1).each do |i|
-    index_min = array[i..array.length].index(array[i..array.length].min) # index o minimum element in array
+    index_min = array[i..array.length].index(array[i..array.length].min) + i # index o minimum element in array, the "+ i" makes shure that i will not take in consideration the already sorted terms
     # array[i..array.length][0],array[i..array.length][index_min] = array[i..array.length][index_min],array[i..array.length][0]
-    array[0],array[index_min] = array[index_min],array[0]
+    array[i],array[index_min] = array[index_min],array[i]
     p array
 
 
@@ -13,5 +12,5 @@ def selection_sort(array)
   p array
 end
 
-array = [64, 25, 12, 22, 11]
+array = [64, 25, 789, 12, 22, 11]
 selection_sort(array)
