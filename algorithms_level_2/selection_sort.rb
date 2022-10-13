4347537df8b33ccc5dee3a076501c4dbeb09209e
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-13 08:33:12 -0300

    feat(selection_sort): don't understand why array isn't changing

--- algorithms_level_2/selection_sort.rb
+++ algorithms_level_2/selection_sort.rb
@@ -1,10 +1,14 @@
 def selection_sort(array)
 
+
   (0..array.length-1).each do |i|
+    index_min = array[i..array.length].index(array[i..array.length].min) # index o minimum element in array
+    array[i..array.length][0],array[i..array.length][index_min] = array[i..array.length][index_min],array[i..array.length][0]
+    p array
 
 
   end
-
+  p array
 end
 
 array = [64, 25, 12, 22, 11]
