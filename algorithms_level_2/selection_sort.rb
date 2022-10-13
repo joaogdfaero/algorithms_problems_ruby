17acdfb04fe9f2bb2bd315861a2090ec196a1bc6
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-13 08:52:55 -0300

    feat(selection_sort): run debug to see whats happening

--- algorithms_level_2/selection_sort.rb
+++ algorithms_level_2/selection_sort.rb
@@ -3,8 +3,10 @@ def selection_sort(array)
 
   (0..array.length-1).each do |i|
     index_min = array[i..array.length].index(array[i..array.length].min) # index o minimum element in array
-    array[i..array.length][0],array[i..array.length][index_min] = array[i..array.length][index_min],array[i..array.length][0]
+    # array[i..array.length][0],array[i..array.length][index_min] = array[i..array.length][index_min],array[i..array.length][0]
+    array[0],array[index_min] = array[index_min],array[0]
     p array
+
 
 
   end
