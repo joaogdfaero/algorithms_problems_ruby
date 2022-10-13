f1884e6484843ec0956bf3c5dccaa62eb4d57536
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-13 17:27:10 -0300

    docs(selection_sort): removed useless comment

--- algorithms_level_2/selection_sort.rb
+++ algorithms_level_2/selection_sort.rb
@@ -2,7 +2,6 @@ def selection_sort(array)
 
   (0..array.length-1).each do |i|
     index_min = array[i..array.length].index(array[i..array.length].min) + i # index o minimum element in array, the "+ i" makes shure that i will not take in consideration the already sorted terms
-    # array[i..array.length][0],array[i..array.length][index_min] = array[i..array.length][index_min],array[i..array.length][0]
     array[i],array[index_min] = array[index_min],array[i]
     p array
 
