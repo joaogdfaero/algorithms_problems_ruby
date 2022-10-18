b5aedf017821996ed96cb15ae7398443da89cc21
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-18 15:59:05 -0300

    docs: merge sort explanation and links

---
+++ algorithms_level_2/merge_sort.rb
@@ -0,0 +1,15 @@
+# https://www.geeksforgeeks.org/merge-sort/
+# https://www.youtube.com/watch?v=4VqmGXwpLqc&ab_channel=MichaelSambol
+
+# STEPS:
+# 1. DIVIDE THE ARRAY IN HALF UNTIL WE GET ATOMIC ELEMENTS
+# 2. COMPARE EACH ATOMIC ELEMENT (FROM LEFT TO RIGHT) AND START MERGING ARRAYS
+# 3. DO STEP 2 UNTIL YOU GET THE FULL COMPLETED SORTED ARRAY
+
+def merge_sort(input)
+
+
+end
+
+
+input = [38, 27, 43, 3, 9, 82, 10]
