729dac287ed26ec697223c34cc39b146660698ba
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-18 16:11:34 -0300

    feat: wrote first, last and mid index

--- algorithms_level_2/merge_sort.rb
+++ algorithms_level_2/merge_sort.rb
@@ -7,9 +7,15 @@
 # 3. DO STEP 2 UNTIL YOU GET THE FULL COMPLETED SORTED ARRAY
 
 def merge_sort(input)
+  first_index = 0
+  last_index = input.length-1
+  mid_index = (first_index + last_index -1)/2
+
+
+
 
 
 end
 
-
 input = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(input)
