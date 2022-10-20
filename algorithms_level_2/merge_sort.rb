da3003bd63bd751ad29a74d4f129e05ab14f6a92
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-20 08:36:55 -0300

    feat: wrote all the code

--- algorithms_level_2/merge_sort.rb
+++ algorithms_level_2/merge_sort.rb
@@ -3,6 +3,53 @@
 
 # GITHUB SOLUTION https://gist.github.com/bih/9726300
 
+class MergeSortAlgorithm
+  def sort(numbers)  # Break's the array down into two numbers (number A and number B) and sorts them.
+    if numbers.size <= 1
+      return numbers
+    end
+
+    array_size = numbers.size
+    half_of_size = (array_size/2).round
+
+    left_array = numbers.take(half_of_size)
+    right_array = numbers.drop(half_of_size)
+
+    sorted_left_array = sort(left_array)
+    sorted_right_array = sort(right_array)
+
+    merge(sorted_left_array,sorted_right_array)
+  end
+
+  def merge(left_array, right_array)
+    if right_array.empty?
+      return left_array # We have nothing to compare. Left wins.
+    end
+
+    if left_array.empty?
+      return right_array
+    end
+
+    smallest_number = if left_array.first <= right_array.first
+                        left_array.shift
+                      else
+                        right_array.shift
+                      end
+    # We keep doing it until the left or right array is empty.
+    recursive = merge(left_array,right_array)
+
+    # Okay, either left or right array are empty at this point. So we have a result.
+    [smallest_number].concat(recursive)
+
+
+  end
+
+
+
+end
+
+merge_sort = MergeSortAlgorithm.new
+puts merge_sort.sort([4, 92, 1, 39, 19, 93, 49, 10].shuffle)
 
 
 # How it works
