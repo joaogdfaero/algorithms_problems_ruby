ad461a7d59acf27676d0d8ad2b493a449f34e487
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-25 07:46:22 -0300

    feat: finished merge sort simple

--- algorithms_level_2/merge_sort_simple.rb
+++ algorithms_level_2/merge_sort_simple.rb
@@ -13,11 +13,19 @@ def merge_sort(unsorted_array)
   end
 end
 
-def merge(left_array, right_array)
+def merge(left_array, right_array) # method that given two arrays can merge them into one array in order.
   sorted_array = []
   # If either array is empty we don't need to compare them
 
-  while !left_array.empty? && !right_array.empty? do
-
+  while !left_array.empty? && !right_array.empty? do # while the arrays are not empty
+    # we are shifting out the compared/used values so we don't repeat
+    if left_array[0] < right_array[0]
+      sorted_array.push(left_array.shift)
+    else
+      sorted_array.push(right_array.shift)
+    end
   end
+
+  #concat appends elements of another array to an array
+  return sorted_array.concat(left_array).concat(right_array)
 end
