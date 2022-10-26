30419351a99bde4e9f2481a6852b92872ce994e4
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-26 16:03:47 -0300

    feat: first sorting works

--- algorithms_level_2/quick_sort.rb
+++ algorithms_level_2/quick_sort.rb
@@ -9,7 +9,7 @@ def quick_sort(unsorted_array)
   # make "i" the smaller element
   # if "j" is lesser than pivot, change i with j
 
-  # make 70 the pivot element
+  # make the last element the pivot element
   pivot_index = unsorted_array.length - 1
 
   i = -1 # i starts with as -1
@@ -19,16 +19,22 @@ def quick_sort(unsorted_array)
     if unsorted_array[j] <= unsorted_array[pivot_index]
       # if "j" is lesser than pivot, change i with j
       i = i + 1
-      unsorted_array[j] , unsorted_array [i] = unsorted_array[i] , unsorted_array [j]
+      unsorted_array[j],unsorted_array[i] = unsorted_array[i], unsorted_array[j]
     end
   end
 
   p unsorted_array
 
-  # swap 70 with 50
+  # swap i+1 with pivot
   unsorted_array[pivot_index],unsorted_array[i+1] = unsorted_array[i+1], unsorted_array[pivot_index]
 
   p unsorted_array
+
+  # apply quick-sort to left and right part (recursion?)
+  #recursive = quick_sort(unsorted_array)
+
+  #p "recursive is #{recursive}"
+
 end
 
 unsorted_array = [10, 80, 30, 90, 40, 50, 70]
