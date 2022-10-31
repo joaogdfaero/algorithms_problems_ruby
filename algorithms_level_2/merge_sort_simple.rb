ade47429fa4cc906edc8b2b972e495e01fc094bf
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-31 15:40:00 -0300

    style(add class and method):

--- algorithms_level_2/merge_sort_simple.rb
+++ algorithms_level_2/merge_sort_simple.rb
@@ -1,35 +1,40 @@
 # REFERENCE: https://medium.com/@allegranzia/merge-sort-in-ruby-102b750af287
 
-def merge_sort(unsorted_array)
-  # if array only has one element or fewer there is nothing to do except print
-  if unsorted_array.length <=1
-    return unsorted_array
-  else
-    # divding and then merge-sorting the halves
-    mid = unsorted_array.length/2
-    first_half = merge_sort(unsorted_array.slice(0...mid)) # recursion to call himselt to divide again
-    second_half = merge_sort(unsorted_array.slice(mid...unsorted_array.length)) # recursion to call himself to divide again
-    merge(first_half,second_half)
-  end
-end
-
-def merge(left_array, right_array) # method that given two arrays can merge them into one array in order.
-  sorted_array = []
-  # If either array is empty we don't need to compare them
-
-
-  while !left_array.empty? && !right_array.empty? do # while the arrays are not empty
-    # we are shifting out the compared/used values so we don't repeat
-    if left_array[0] < right_array[0]
-      sorted_array.push(left_array.shift)
+class Algorithms_Level2
+  def merge_sort(unsorted_array)
+    # if array only has one element or fewer there is nothing to do except print
+    if unsorted_array.length <=1
+      return unsorted_array
     else
-      sorted_array.push(right_array.shift)
+      # divding and then merge-sorting the halves
+      mid = unsorted_array.length/2
+      first_half = merge_sort(unsorted_array.slice(0...mid)) # recursion to call himselt to divide again
+      second_half = merge_sort(unsorted_array.slice(mid...unsorted_array.length)) # recursion to call himself to divide again
+      merge(first_half,second_half)
     end
   end
 
-  #concat appends elements of another array to an array
-  # concatenates all elements from left or right array who whore not shifted (deleted from array and pushed to sorted_array) in while loop line 20
-  return sorted_array.concat(left_array).concat(right_array)
+  def merge(left_array, right_array) # method that given two arrays can merge them into one array in order.
+    sorted_array = []
+    # If either array is empty we don't need to compare them
+
+
+    while !left_array.empty? && !right_array.empty? do # while the arrays are not empty
+      # we are shifting out the compared/used values so we don't repeat
+      if left_array[0] < right_array[0]
+        sorted_array.push(left_array.shift)
+      else
+        sorted_array.push(right_array.shift)
+      end
+    end
+
+    #concat appends elements of another array to an array
+    # concatenates all elements from left or right array who whore not shifted (deleted from array and pushed to sorted_array) in while loop line 20
+    return sorted_array.concat(left_array).concat(right_array)
+  end
 end
 
-p merge_sort([33,4,500,1,2])
+merge_sort = Algorithms_Level2.new
+p merge_sort.merge_sort([33,4,500,1,2])
+
+
