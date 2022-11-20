79f299e7478a7d719d1e467032fdcc92dd8f8232
João Gabriel Dal Forno <103076610+joaogdfaero@users.noreply.github.com>
2022-11-20 19:12:54 -0300

    Update minimum_sum.rb

--- algorithms_level_1/minimum_sum.rb
+++ algorithms_level_1/minimum_sum.rb
@@ -1,14 +1,5 @@
 # https://www.geeksforgeeks.org/minimum-sum-choosing-minimum-pairs-a/ (read kujikuji comment for better explanation)
 
-# form two digit numbers in a way the the sum of this two numbers is minimum
-
-#Input : a[] = {2, 4, 1, 3}
-#Output : 3
-
-# So the minimum sum can always be (minimum of a[]) * (size of b[] (which equals n-1)).
-
-
-
 def minimum_sum(a)
     p "a = #{a}"
 
@@ -16,7 +7,6 @@ def minimum_sum(a)
     
     while a.length != 1
         p "a tem um tamanho #{a.length}"
-        #(0..a.length).each do |i| -> THIS IS MAKING THE LOOP TO RUN MORE TIMES THAN IT NEEDED, RESULTING ON ERROS
             if a[0]>a[1]
                 a.delete_at(0)
                 b.append(a[1])
@@ -34,12 +24,7 @@ def minimum_sum(a)
             end
     end
     
-    p "a final é"
-    p a
-    p "b final é"
-    p b
-
-    p "O resultado final é #{a[0]*b.length}"
+    p "Final result is #{a[0]*b.length}"
 
 end
 
