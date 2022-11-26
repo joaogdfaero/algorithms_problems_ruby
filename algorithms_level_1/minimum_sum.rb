4cfde514b09c78847fc26c3de9ac3232d2c6e568
João Gabriel Dal Forno <103076610+joaogdfaero@users.noreply.github.com>
2022-11-26 10:53:17 -0300

    Update minimum_sum.rb

--- algorithms_level_1/minimum_sum.rb
+++ algorithms_level_1/minimum_sum.rb
@@ -1,29 +1,19 @@
 # https://www.geeksforgeeks.org/minimum-sum-choosing-minimum-pairs-a/ (read kujikuji comment for better explanation)
 
 def minimum_sum(a)
-    p "a = #{a}"
     b =[]
     
     while a.length != 1
-        p "a tem um tamanho #{a.length}"
             if a[0]>a[1]
                 a.delete_at(0)
-                b.append(a[1])
-                p "b é:"
-                p b
-                p "a é"
-                p a
-            elsif a[0]<a[1]
+                b.append(a[1])" 
+            elsif a[0] < a[1]
                 a.delete_at(1)
                 b.append(a[0])
-                p "b é:"
-                p b
-                p "a é"
-                p a
             end
     end
     
-    p "Final result is #{a[0]*b.length}"
+    return a[0]*b.length
 
 end
 
