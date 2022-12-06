04dbb7c752ced20ad1adf263386f858639cdae79
João Gabriel Dal Forno <103076610+joaogdfaero@users.noreply.github.com>
2022-12-06 12:52:58 -0300

    Update find_maximum.rb

--- algorithms_level_1/find_maximum.rb
+++ algorithms_level_1/find_maximum.rb
@@ -1,5 +1,11 @@
 # https://www.geeksforgeeks.org/find-maximum-sum-possible-equal-sum-three-stacks/
 
+# ALGORITHM
+    #1. Find the sum of all elements of in individual stacks.
+    #2. If the sum of all three stacks is the same, then this is the maximum sum.
+    #3. Else remove the top element of the stack having the maximum sum among three of stacks. 
+    #4. Repeat step 1 and step 2.
+
 def find_maximum(a,b,c)
     sum_a = 0
     (0..a.length-1).each do |i|
@@ -40,15 +46,13 @@ def find_maximum(a,b,c)
             end
 
             sums = [sum_a,sum_b,sum_c]
-            p "a soma máxima é #{sums.max}"
+            p "Maximum sum is #{sums.max}"
 
             if sum_a == sum_b && sum_b == sum_c
-                p "A soma é #{sum_a} "
+                p "Maximum sum is #{sum_a} "
                 break
             elsif sums[0]==sums.max
                 a.delete_at(0)
-                p "0"
-                p a
             elsif sums[1]==sums.max
                 b.delete_at(0)
                 p "1"
@@ -69,15 +73,6 @@ def find_maximum(a,b,c)
             
         
     end
-
-
-
-
-    #Find the sum of all elements of in individual stacks.
-    #If the sum of all three stacks is the same, then this is the maximum sum.
-    #Else remove the top element of the stack having the maximum sum among three of stacks. 
-    #Repeat step 1 and step 2.
-
 end
 
 
