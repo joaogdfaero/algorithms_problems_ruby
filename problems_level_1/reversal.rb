ea682d4773981f80e5b5e2de5b4adbd669f706ee
João Gabriel Dal Forno <103076610+joaogdfaero@users.noreply.github.com>
2022-12-20 16:40:50 -0300

    Update reversal.rb

--- problems_level_1/reversal.rb
+++ problems_level_1/reversal.rb
@@ -1,13 +1,10 @@
 # Reversal algorithm for right rotation of an array
 
-# Given an array, right rotate it by k elements. asd ""
+# Given an array, right rotate it by k elements.
 
 # Input: arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 # k = 3
 # Output: 8 9 10 1 2 3 4 5 6 7
-
-# O MÉTODO DE FAZER UM NÚMERO NA FRENTE VIR
-
 
 def reversal(list,k)
     p "A list inicialmente é"
