495c458a724e7d5b9faebb2978678c6e82b2d404
João Gabriel Dal Forno <103076610+joaogdfaero@users.noreply.github.com>
2023-01-02 21:33:01 -0300

    Update minimum_sum.rb

--- algorithms_level_1/minimum_sum.rb
+++ algorithms_level_1/minimum_sum.rb
@@ -10,12 +10,13 @@ def minimum_sum(a)
             elsif a[0] < a[1]
                 a.delete_at(1)
                 b.append(a[0])
-            end
+           end
     end
     
     return a[0]*b.length
 end
 
+#
 a = [2,4,1,3]
 minimum_sum(a)
 
